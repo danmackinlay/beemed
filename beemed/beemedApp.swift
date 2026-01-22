470fbbd34a3f063f1a3097335d4255eb642c8e4b
dan mackinlay <dan@livingthing.org>
2026-01-22 12:48:56 +0000

    Initial Commit

---
+++ beemed/beemedApp.swift
@@ -0,0 +1,32 @@
+//
+//  beemedApp.swift
+//  beemed
+//
+//  Created by Daniel Mackinlay on 22/1/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct beemedApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
